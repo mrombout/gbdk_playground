bdd1bd2e0bbdca53732bdfb9098e8b257540b805
Mike Rombout <mike.rombout1@gmail.com>
2016-09-18 10:38:50 +0200

    Create window sample

---
+++ window/README.md
@@ -0,0 +1,7 @@
+# Window
+
+![](screenshot.png)
+
+Demonstrates the use of the window layer along with the background layer. Both
+use similar API.
+
---
+++ window/main.c
@@ -0,0 +1,19 @@
+#include <gb/gb.h>
+
+#include "bg.c"
+#include "bg.map"
+#include "border.c"
+#include "window.c"
+
+void main() {
+    set_bkg_data(0, 152, bg_tiledata);
+    set_bkg_tiles(0, 0, 20, 18, bg_tilemap);
+    
+    SHOW_BKG;
+    
+    set_win_data(152, 9, border);
+    set_win_tiles(0, 0, 20, 4, window);
+    move_win(7, 112);
+
+    SHOW_WIN;
+}
