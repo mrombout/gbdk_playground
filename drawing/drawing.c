35c45e6bcf875a671a8865800e0860ff6af39527
fri5by <frisbyjoshua@gmail.com>
2019-03-28 12:55:16 -0400

    Add Font and Save RAM examples
    Add two new examples and tidy up documentation.

--- drawing/drawing.c
+++ drawing/drawing.c
@@ -4,13 +4,13 @@
 UINT8 x = 1, y = 1, i = 3, j, f;
 
 void main() {
-	color(BLACK, WHITE, SOLID);
-	circle(100, 60, 30, M_FILL);
-	line(40, 40, 50, 50);
-	box(120, 125, 125, 135, M_NOFILL);
-	plot_point(3, 4);
-	gotogxy(18, 15);
-	gprintf(":)");
+	color(BLACK, WHITE, SOLID); //  forecolor, backcolor, mode [Colors: WHITE (0), LTGREY (1), DKGREY(2), BLACK (3)]
+	circle(100, 60, 30, M_FILL); // x, y, radius, style (M_FILL or M_NOFILL)
+	line(40, 40, 50, 50); // x1, y1, x2, y2
+	box(120, 125, 125, 135, M_NOFILL); // x1, y1, x2, y2, style (M_FILL or M_NOFILL)
+	plot_point(3, 4); // Plot a single pixel on the screen
+	gotogxy(18, 15); // Places you at these coordinates
+	gprintf("Hi"); // When using the drawing library you must use gprintf instead of printf
 
 	while (1) {
 		if (joypad() == J_UP && y != 0)
@@ -21,21 +21,21 @@ void main() {
 			x--;
 		if (joypad() == J_RIGHT && x != 159)
 			x++;
-		if (joypad() == J_A) {
+		if (joypad() == J_A) { // Change colors
 			waitpadup();
 			i++;
 			if (i > 3)
 				i = 0;
 		}
-		if (joypad() == J_B) {
-			for (j = 0; j < 20; j++) {
-				for (f = 0; f < 18; f++) {
+		if (joypad() == J_B) { // Function to 'clear' the screen
+			for (j = 0; j < 20; j++) { // GB screen is 20 columns wide and 18 columns tall
+				for (f = 0; f < 18; f++) { 
 					gotogxy(j, f);
-					wrtchr(' ');
+					wrtchr(' '); // Use wrtchr to place a character when using the drawing library
 				}
 			}
 		}
-		plot(x, y, i, SOLID);
+		plot(x, y, i, SOLID); // Draws a pixel on the screen (x, y, color, mode)
 		delay(10);
 	}
 }
---
+++ save_ram/README.md
@@ -0,0 +1,5 @@
+# Save RAM
+
+![](screenshot.png)
+
+Demonstrates how to load and save variables. Using Save RAM requires additional compiler arguments.
